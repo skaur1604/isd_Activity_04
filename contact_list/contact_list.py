19b5c02bb56769f6ed184a446effee4983023296
skaur1604 <skaur1604@academic.rrc.ca>
2025-11-06 18:14:39 -0600

    Implement on_add_contact slot to add contact records to table

--- contact_list/contact_list.py
+++ contact_list/contact_list.py
@@ -2,9 +2,12 @@
 
 __author__ = "ACE Faculty"
 __version__ = "1.0.0"
-__credits__ = ""
+__credits__ = "Sukhpreet Kaur"
 
-from PySide6.QtWidgets import  QMainWindow, QLineEdit, QPushButton, QTableWidget, QLabel, QVBoxLayout, QWidget, QTableWidgetItem
+from PySide6.QtWidgets import  (QMainWindow, QLineEdit, QPushButton,
+                                QTableWidget, QLabel, QVBoxLayout, 
+                                QWidget, QTableWidgetItem)
+from PySide6.QtCore import Slot
 
 class ContactList(QMainWindow):
     """Represents a window that provides the UI to manage contacts."""
@@ -13,7 +16,8 @@ class ContactList(QMainWindow):
         """Initializes a new instance of the ContactList class."""
 
         super().__init__()
-        self.__initialize_widgets()      
+        self.__initialize_widgets()  
+        self.add_button.clicked.connect(self.__on_add_contact)      
 
     def __initialize_widgets(self):
         """Initializes the widgets on this Window.
@@ -48,3 +52,29 @@ class ContactList(QMainWindow):
         container = QWidget()
         container.setLayout(layout)
         self.setCentralWidget(container)
+
+    @Slot()
+    def __on_add_contact(self):
+
+        """This will handle the add contact button click event"""
+
+        name = self.contact_name_input.text().strip()
+        phone = self.phone_input.text().strip()
+
+        if len(name) > 0 and len(phone) > 0:
+            row_position = self.contact_table.rowCount()
+            self.contact_table.insertRow(row_position)
+            name_item = QTableWidgetItem(name)
+            self.contact_table.setItem(row_position, 0,
+                                        QTableWidgetItem(name_item))
+            phone_item = QTableWidgetItem(phone)
+            self.contact_table.setItem(row_position, 1,
+                                    QTableWidgetItem(phone_item))
+
+            self.status_label.setText(f"Added contact: {name}")
+            self.contact_name_input.clear()
+            self.phone_input.clear()
+        else:
+            self.status_label.setText(f"Please enter a contact "
+                                      f"name and phone number.")
+
